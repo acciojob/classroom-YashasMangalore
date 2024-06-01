a4b3adcc8d26a19a6b09d9c832e0c86e9a24ecc0
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-06-01 18:04:32 +0530

    second commit

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -11,6 +11,13 @@ public class Teacher {
     public Teacher() {
     }
 
+    public Teacher(String name, int numberOfStudents, int age)
+    {
+        this.name = name;
+        this.numberOfStudents = numberOfStudents;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
